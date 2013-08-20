b51d798a6e6f8b40ee1d350e18e3b655dd01e20c
Saurav R Tuladhar <sauravtuladhar@gmail.com>
2013-08-19 23:01:53 -0400

    Refactored. BS uses lxml. Temp parse logic in own function.

--- nepal_mfd_scrape.py
+++ nepal_mfd_scrape.py
@@ -26,43 +26,55 @@ def get_ktm_temp_data(y):
             url = 'http://www.mfd.gov.np/archivereport.php?year=' + \
                   str(y) + '&month=' + str(m)+'&day=' + str(d)
             page = urllib2.urlopen(url)
-            #you might want to pass a parser to BeautifulSoup. The default parser does not parse malformed html pages correctly.
-            #apt-get install python-lxml
-            #soup = BeautifulSoup(page, "lxml")
-            soup = BeautifulSoup(page)
-
-            #### Please try to separate the parsing logic to a different function. This will make the code easy to maintain if ever the structure of the page changes.
-            citytag  = soup.find_all('td', text=re.compile("Kathmandu "))
-            try:
-                max_temp_tag = citytag[0].find_next('td')
-            except:
-                continue
-            min_temp_tag =  max_temp_tag.find_next('td')
-
-            #### Forecasted temperature
-            btag = soup.find_all('b')
-            fmintemp = str(btag[9].get_text())
-            sepidx = fmintemp.find('-', 1)
-            fmin_temp_lo = (fmintemp[:sepidx])
-            fmin_temp_hi = (fmintemp[sepidx+1:])
-                
-            fmaxtemp = str(btag[11].get_text())
-            sepidx = fmaxtemp.find('-', 1)                          
-            fmax_temp_lo = (fmaxtemp[:sepidx])
-            fmax_temp_hi = (fmaxtemp[sepidx+1:])
-        
-            #### Also you might want to delegate this to a different function (collect the data in some data structure and pass it to the function)
+            soup = BeautifulSoup(page, "lxml")
+            temp_data = get_temp_data(soup)
             print 'Writing data for' + str(m)+ "/" + str(d)
-            f.write(fmin_temp_lo + ',' + \
-                    fmin_temp_hi + ',' + \
-                    fmax_temp_lo + ',' + \
-                    fmax_temp_hi + ',' + \
-                    max_temp_tag.text + ',' + \
-                    min_temp_tag.text + '\n')
+            write_temp_data(f, temp_data)
     f.close()
-    print 'Closed'    
+    print 'Closed'
 
+    # -----------------------------------------------------------------------
 
+def get_temp_data(soup):
+    '''
+    Get temperature data from the soup
+    '''
+    citytag  = soup.find_all('td', text=re.compile("Kathmandu "))
+    # Try to get the tag. Sometimes the data is missing resulting in Out of Index error. In that case just ignore and continue.
+    try:
+        max_temp_tag = citytag[0].find_next('td')
+        data = True
+    except:
+        data = False
+    temp_data = {}
+    if data:
+        temp_data['max_temp'] = max_temp_tag.text        
+        min_temp_tag =  max_temp_tag.find_next('td')
+        temp_data['min_temp'] = min_temp_tag.text        
+        #### Forecasted temperature
+        btag = soup.find_all('b')
+        fmintemp = str(btag[9].get_text())
+        sepidx = fmintemp.find('-', 1)
+        temp_data['fmin_temp_lo'] = (fmintemp[:sepidx])
+        temp_data['fmin_temp_hi'] = (fmintemp[sepidx+1:])
+        fmaxtemp = str(btag[11].get_text())
+        sepidx = fmaxtemp.find('-', 1)                          
+        temp_data['fmax_temp_lo'] = (fmaxtemp[:sepidx])
+        temp_data['fmax_temp_hi'] = (fmaxtemp[sepidx+1:])
+        
+    return temp_data
+
+    # -----------------------------------------------------------------------
+    
+def write_temp_data(fh, temp_data):
+    # Write temp_data to csv file
+    fh.write(temp_data['fmin_temp_lo'] + ',' + \
+            temp_data['fmin_temp_hi'] + ',' + \
+            temp_data['fmax_temp_lo'] + ',' + \
+            temp_data['fmax_temp_hi'] + ',' + \
+            temp_data['max_temp'] + ',' + \
+            temp_data['min_temp'] + '\n')
+    # -----------------------------------------------------------------------
 
 if __name__ == "__main__":
     get_ktm_temp_data(2012)
